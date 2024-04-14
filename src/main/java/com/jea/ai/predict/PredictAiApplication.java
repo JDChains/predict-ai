6017969ddf39d9ec850f90840b0239da89968692
Jagadeesh <blockchian@jeadigital.com>
2024-04-14 10:54:47 +0530

    Predict AI Spring boot  project created

---
+++ src/main/java/com/jea/ai/predict/PredictAiApplication.java
@@ -0,0 +1,13 @@
+package com.jea.ai.predict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictAiApplication.class, args);
+	}
+
+}
